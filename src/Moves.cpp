8e569b9141d1cb959d7a4246c1ee531910e603c0
risingPhil <philippe.symons@gmail.com>
2024-07-23 22:54:53 +0200

    Fix capitalization

--- src/Moves.cpp
+++ src/Moves.cpp
@@ -7,37 +7,37 @@ const char *moveToString(Move move)
     case Move::POUND:
         return "Pound";
     case Move::KARATE_CHOP:
-        return "Karate chop";
+        return "Karate Chop";
     case Move::DOUBLE_SLAP:
-        return "Double slap";
+        return "Double Slap";
     case Move::COMET_PUNCH:
-        return "Comet punch";
+        return "Comet Punch";
     case Move::MEGA_PUNCH:
-        return "Mega punch";
+        return "Mega Punch";
     case Move::PAY_DAY:
-        return "Pay day";
+        return "Pay Day";
     case Move::FIRE_PUNCH:
-        return "Fire punch";
+        return "Fire Punch";
     case Move::ICE_PUNCH:
-        return "Ice punch";
+        return "Ice Punch";
     case Move::THUNDER_PUNCH:
-        return "Thunder punch";
+        return "Thunder Punch";
     case Move::SCRATCH:
         return "Scratch";
     case Move::VICE_GRIP:
-        return "Vice grip";
+        return "Vice Grip";
     case Move::GUILLOTINE:
         return "Guillotine";
     case Move::RAZOR_WIND:
-        return "Razor wind";
+        return "Razor Wind";
     case Move::SWORDS_DANCE:
-        return "Swords dance";
+        return "Swords Dance";
     case Move::CUT:
         return "Cut";
     case Move::GUST:
         return "Gust";
     case Move::WING_ATTACK:
-        return "Wing attack";
+        return "Wing Attack";
     case Move::WHIRLWIND:
         return "Whirlwind";
     case Move::FLY:
@@ -47,47 +47,47 @@ const char *moveToString(Move move)
     case Move::SLAM:
         return "Slam";
     case Move::VINE_WHIP:
-        return "Vine whip";
+        return "Vine Whip";
     case Move::STOMP:
         return "Stomp";
     case Move::DOUBLE_KICK:
-        return "Double kick";
+        return "Double Kick";
     case Move::MEGA_KICK:
-        return "Mega kick";
+        return "Mega Kick";
     case Move::JUMP_KICK:
-        return "Jump kick";
+        return "Jump Kick";
     case Move::ROLLING_KICK:
-        return "Rolling kick";
+        return "Rolling Kick";
     case Move::SAND_ATTACK:
-        return "Sand attack";
+        return "Sand Attack";
     case Move::HEADBUTT:
         return "Headbutt";
     case Move::HORN_ATTACK:
-        return "Horn attack";
+        return "Horn Attack";
     case Move::FURY_ATTACK:
-        return "Fury attack";
+        return "Fury Attack";
     case Move::HORN_DRILL:
-        return "Horn drill";
+        return "Horn Drill";
     case Move::TACKLE:
         return "Tackle";
     case Move::BODY_SLAM:
-        return "Body slam";
+        return "Body Slam";
     case Move::WRAP:
         return "Wrap";
     case Move::TAKE_DOWN:
-        return "Take down";
+        return "Take Down";
     case Move::THRASH:
         return "Thrash";
     case Move::DOUBLE_EDGE:
-        return "Double edge";
+        return "Double Edge";
     case Move::TAIL_WHIP:
-        return "Tail whip";
+        return "Tail Whip";
     case Move::POISON_STING:
-        return "Poison sting";
+        return "Poison Sting";
     case Move::TWINEEDLE:
         return "Twineedle";
     case Move::PIN_MISSILE:
-        return "Pin missile";
+        return "Pin Missile";
     case Move::LEER:
         return "Leer";
     case Move::BITE:
@@ -101,7 +101,7 @@ const char *moveToString(Move move)
     case Move::SUPERSONIC:
         return "Supersonic";
     case Move::SONIC_BOOM:
-        return "Sonic boom";
+        return "Sonic Boom";
     case Move::DISABLE:
         return "Disable";
     case Move::ACID:
@@ -113,73 +113,73 @@ const char *moveToString(Move move)
     case Move::MIST:
         return "Mist";
     case Move::WATER_GUN:
-        return "Water gun";
+        return "Water Gun";
     case Move::HYDRO_PUMP:
-        return "Hydro pump";
+        return "Hydro Pump";
     case Move::SURF:
         return "Surf";
     case Move::ICE_BEAM:
-        return "Ice beam";
+        return "Ice Beam";
     case Move::BLIZZARD:
         return "Blizzard";
     case Move::PSYBEAM:
         return "Psybeam";
     case Move::BUBBLE_BEAM:
-        return "Bubble beam";
+        return "Bubble Beam";
     case Move::AURORA_BEAM:
-        return "Aurora beam";
+        return "Aurora Beam";
     case Move::HYPER_BEAM:
-        return "Hyper beam";
+        return "Hyper Beam";
     case Move::PECK:
         return "Peck";
     case Move::DRILL_PECK:
-        return "Drill peck";
+        return "Drill Peck";
     case Move::SUBMISSION:
         return "Submission";
     case Move::LOW_KICK:
-        return "Low kick";
+        return "Low Kick";
     case Move::COUNTER:
         return "Counter";
     case Move::SEISMIC_TOSS:
-        return "Seismic toss";
+        return "Seismic Toss";
     case Move::STRENGTH:
         return "Strength";
     case Move::ABSORB:
         return "Absorb";
     case Move::MEGA_DRAIN:
-        return "Mega drain";
+        return "Mega Drain";
     case Move::LEECH_SEED:
-        return "Leech seed";
+        return "Leech Seed";
     case Move::GROWTH:
         return "Growth";
     case Move::RAZOR_LEAF:
-        return "Razor leaf";
+        return "Razor Leaf";
     case Move::SOLAR_BEAM:
-        return "Solar beam";
+        return "Solar Beam";
     case Move::POISON_POWDER:
-        return "Poison powder";
+        return "Poison Powder";
     case Move::STUN_SPORE:
-        return "Stun spore";
+        return "Stun Spore";
     case Move::SLEEP_POWDER:
-        return "Sleep powder";
+        return "Sleep Powder";
     case Move::PETAL_DANCE:
-        return "Petal dance";
+        return "Petal Dance";
     case Move::STRING_SHOT:
-        return "String shot";
+        return "String Shot";
     case Move::DRAGON_RAGE:
-        return "Dragon rage";
+        return "Dragon Rage";
     case Move::FIRE_SPIN:
-        return "Fire spin";
+        return "Fire Spin";
     case Move::THUNDER_SHOCK:
-        return "Thunder shock";
+        return "Thunder Shock";
     case Move::THUNDERBOLT:
         return "Thunderbolt";
     case Move::THUNDER_WAVE:
-        return "Thunder wave";
+        return "Thunder Wave";
     case Move::THUNDER:
         return "Thunder";
     case Move::ROCK_THROW:
-        return "Rock throw";
+        return "Rock Throw";
     case Move::EARTHQUAKE:
         return "Earthquake";
     case Move::FISSURE:
@@ -199,19 +199,19 @@ const char *moveToString(Move move)
     case Move::AGILITY:
         return "Agility";
     case Move::QUICK_ATTACK:
-        return "Quick attack";
+        return "Quick Attack";
     case Move::RAGE:
         return "Rage";
     case Move::TELEPORT:
         return "Teleport";
     case Move::NIGHT_SHADE:
-        return "Night shade";
+        return "Night Shade";
     case Move::MIMIC:
         return "Mimic";
     case Move::SCREECH:
         return "Screech";
     case Move::DOUBLE_TEAM:
-        return "Double team";
+        return "Double Team";
     case Move::RECOVER:
         return "Recover";
     case Move::HARDEN:
@@ -221,31 +221,31 @@ const char *moveToString(Move move)
     case Move::SMOKESCREEN:
         return "Smokescreen";
     case Move::CONFUSE_RAY:
-        return "Confuse ray";
+        return "Confuse Ray";
     case Move::WITHDRAW:
         return "Withdraw";
     case Move::DEFENSE_CURL:
-        return "Defense curl";
+        return "Defense Curl";
     case Move::BARRIER:
         return "Barrier";
     case Move::LIGHT_SCREEN:
-        return "Light screen";
+        return "Light Screen";
     case Move::HAZE:
         return "Haze";
     case Move::REFLECT:
         return "Reflect";
     case Move::FOCUS_ENERGY:
-        return "Focus energy";
+        return "Focus Energy";
     case Move::BIDE:
         return "Bide";
     case Move::METRONOME:
         return "Metronome";
     case Move::MIRROR_MOVE:
-        return "Mirror move";
+        return "Mirror Move";
     case Move::SELF_DESTRUCT:
-        return "Self destruct";
+        return "Self Destruct";
     case Move::EGG_BOMB:
-        return "Egg bomb";
+        return "Egg Bomb";
     case Move::LICK:
         return "Lick";
     case Move::SMOG:
@@ -253,9 +253,9 @@ const char *moveToString(Move move)
     case Move::SLUDGE:
         return "Sludge";
     case Move::BONE_CLUB:
-        return "Bone club";
+        return "Bone Club";
     case Move::FIRE_BLAST:
-        return "Fire blast";
+        return "Fire Blast";
     case Move::WATERFALL:
         return "Waterfall";
     case Move::CLAMP:
@@ -263,9 +263,9 @@ const char *moveToString(Move move)
     case Move::SWIFT:
         return "Swift";
     case Move::SKULL_BASH:
-        return "Skull bash";
+        return "Skull Bash";
     case Move::SPIKE_CANNON:
-        return "Spike cannon";
+        return "Spike Cannon";
     case Move::CONSTRICT:
         return "Constrict";
     case Move::AMNESIA:
@@ -273,29 +273,29 @@ const char *moveToString(Move move)
     case Move::KINESIS:
         return "Kinesis";
     case Move::SOFT_BOILED:
-        return "Soft boiled";
+        return "Soft Boiled";
     case Move::HI_JUMP_KICK:
-        return "Hi jump kick";
+        return "Hi Jump Kick";
     case Move::GLARE:
         return "Glare";
     case Move::DREAM_EATER:
-        return "Dream eater";
+        return "Dream Eater";
     case Move::POISON_GASS:
-        return "Poison gass";
+        return "Poison Gass";
     case Move::BARRAGE:
         return "Barrage";
     case Move::LEECH_LIFE:
-        return "Leech life";
+        return "Leech Life";
     case Move::LOVELY_KISS:
-        return "Lovely kiss";
+        return "Lovely Kiss";
     case Move::SKY_ATTACK:
-        return "Sky attack";
+        return "Sky Attack";
     case Move::TRANSFORM:
         return "Transform";
     case Move::BUBBLE:
         return "Bubble";
     case Move::DIZZY_PUNCH:
-        return "Dizzy punch";
+        return "Dizzy Punch";
     case Move::SPORE:
         return "Spore";
     case Move::FLASH:
@@ -305,29 +305,29 @@ const char *moveToString(Move move)
     case Move::SPLASH:
         return "Splash";
     case Move::ACID_ARMOR:
-        return "Acid armor";
+        return "Acid Armor";
     case Move::CRABHAMMER:
         return "Crabhammer";
     case Move::EXPLOSION:
         return "Explosion";
     case Move::FURY_SWIPES:
-        return "Fury swipes";
+        return "Fury Swipes";
     case Move::BONEMERANG:
         return "Bonemerang";
     case Move::REST:
         return "Rest";
     case Move::ROCK_SLIDE:
-        return "Rock slide";
+        return "Rock Slide";
     case Move::HYPER_FANG:
-        return "Hyper fang";
+        return "Hyper Fang";
     case Move::SHARPEN:
         return "Sharpen";
     case Move::CONVERSION:
         return "Conversion";
     case Move::TRI_ATTACK:
-        return "Tri attack";
+        return "Tri Attack";
     case Move::SUPER_FANG:
-        return "Super fang";
+        return "Super Fang";
     case Move::SLASH:
         return "Slash";
     case Move::SUBSTITUTE:
@@ -337,17 +337,17 @@ const char *moveToString(Move move)
     case Move::SKETCH:
         return "Sketch";
     case Move::TRIPLE_KICK:
-        return "Triple kick";
+        return "Triple Kick";
     case Move::THIEF:
         return "Thief";
     case Move::SPIDER_WEB:
-        return "Spider web";
+        return "Spider Web";
     case Move::MIND_READER:
-        return "Mind reader";
+        return "Mind Reader";
     case Move::NIGHTMARE:
         return "Nightmare";
     case Move::FLAME_WHEEL:
-        return "Flame wheel";
+        return "Flame Wheel";
     case Move::SNORE:
         return "Snore";
     case Move::CURSE:
@@ -359,55 +359,55 @@ const char *moveToString(Move move)
     case Move::AEROBLAST:
         return "Aeroblast";
     case Move::COTTON_SPORE:
-        return "Cotton spore";
+        return "Cotton Spore";
     case Move::REVERSAL:
         return "Reversal";
     case Move::SPITE:
         return "Spite";
     case Move::POWDER_SNOW:
-        return "Powder snow";
+        return "Powder Snow";
     case Move::PROTECT:
         return "Protect";
     case Move::MACH_PUNCH:
-        return "Mach punch";
+        return "Mach Punch";
     case Move::SCARY_FACE:
-        return "Scary face";
+        return "Scary Face";
     case Move::FEINT_ATTACK:
-        return "Feint attack";
+        return "Feint Attack";
     case Move::SWEET_KISS:
-        return "Sweet kiss";
+        return "Sweet Kiss";
     case Move::BELLY_DRUM:
-        return "Belly drum";
+        return "Belly Drum";
     case Move::SLUDGE_BOMB:
-        return "Sludge bomb";
+        return "Sludge Bomb";
     case Move::MUD_SLAP:
-        return "Mud slap";
+        return "Mud Slap";
     case Move::OCTAZOOKA:
         return "Octazooka";
     case Move::SPIKES:
         return "Spikes";
     case Move::ZAP_CANNON:
-        return "Zap cannon";
+        return "Zap Cannon";
     case Move::FORESIGHT:
         return "Foresight";
     case Move::DESTINY_BOND:
-        return "Destiny bond";
+        return "Destiny Bond";
     case Move::PERISH_SONG:
-        return "Perish song";
+        return "Perish Song";
     case Move::ICY_WIND:
-        return "Icy wind";
+        return "Icy Wind";
     case Move::DETECT:
         return "Detect";
     case Move::BONE_RUSH:
-        return "Bone rush";
+        return "Bone Rush";
     case Move::LOCK_ON:
-        return "Lock on";
+        return "Lock On";
     case Move::OUTRAGE:
         return "Outrage";
     case Move::SANDSTORM:
         return "Sandstorm";
     case Move::GIGA_DRAIN:
-        return "Giga drain";
+        return "Giga Drain";
     case Move::ENDURE:
         return "Endure";
     case Move::CHARM:
@@ -415,25 +415,25 @@ const char *moveToString(Move move)
     case Move::ROLLOUT:
         return "Rollout";
     case Move::FALSE_SWIPE:
-        return "False swipe";
+        return "False Swipe";
     case Move::SWAGGER:
         return "Swagger";
     case Move::MILK_DRINK:
-        return "Milk drink";
+        return "Milk Drink";
     case Move::SPARK:
         return "Spark";
     case Move::FURY_CUTTER:
-        return "Fury cutter";
+        return "Fury Cutter";
     case Move::STEEL_WING:
-        return "Steel wing";
+        return "Steel Wing";
     case Move::MEAN_LOOK:
-        return "Mean look";
+        return "Mean Look";
     case Move::ATTRACT:
         return "Attract";
     case Move::SLEEP_TALK:
-        return "Sleep talk";
+        return "Sleep Talk";
     case Move::HEAL_BELL:
-        return "Heal bell";
+        return "Heal Bell";
     case Move::RETURN:
         return "Return";
     case Move::PRESENT:
@@ -443,69 +443,69 @@ const char *moveToString(Move move)
     case Move::SAFEGUARD:
         return "Safeguard";
     case Move::PAIN_SPLIT:
-        return "Pain split";
+        return "Pain Split";
     case Move::SACRED_FIRE:
-        return "Sacred fire";
+        return "Sacred Fire";
     case Move::MAGNITUTE:
         return "Magnitute";
     case Move::DYNAMIC_PUNCH:
-        return "Dynamic punch";
+        return "Dynamic Punch";
     case Move::MEGAHORN:
         return "Megahorn";
     case Move::DRAGON_BREATH:
-        return "Dragon breath";
+        return "Dragon Breath";
     case Move::BATON_PASS:
-        return "Baton pass";
+        return "Baton Pass";
     case Move::ENCORE:
         return "Encore";
     case Move::PURSUIT:
         return "Pursuit";
     case Move::RAPID_SPIN:
-        return "Rapid spin";
+        return "Rapid Spin";
     case Move::SWEET_SCENT:
-        return "Sweet scent";
+        return "Sweet Scent";
     case Move::IRON_TAIL:
-        return "Iron tail";
+        return "Iron Tail";
     case Move::METAL_CLAW:
-        return "Metal claw";
+        return "Metal Claw";
     case Move::VITAL_THROW:
-        return "Vital throw";
+        return "Vital Throw";
     case Move::MORNING_SUN:
-        return "Morning sun";
+        return "Morning Sun";
     case Move::SYNTHESIS:
         return "Synthesis";
     case Move::MOONLIGHT:
         return "Moonlight";
     case Move::HIDDEN_POWER:
-        return "Hidden power";
+        return "Hidden Power";
     case Move::CROSS_CHOP:
-        return "Cross chop";
+        return "Cross Chop";
     case Move::TWISTER:
         return "Twister";
     case Move::RAIN_DANCE:
-        return "Rain dance";
+        return "Rain Dance";
     case Move::SUNNY_DAY:
-        return "Sunny day";
+        return "Sunny Day";
     case Move::CRUNCH:
         return "Crunch";
     case Move::MIRROR_COAT:
-        return "Mirror coat";
+        return "Mirror Coat";
     case Move::PSYCH_UP:
-        return "Psych up";
+        return "Psych Up";
     case Move::EXTREME_SPEED:
-        return "Extreme speed";
+        return "Extreme Speed";
     case Move::ANCIENT_POWER:
-        return "Ancient power";
+        return "Ancient Power";
     case Move::SHADOW_BALL:
-        return "Shadow ball";
+        return "Shadow Ball";
     case Move::FUTURE_SIGHT:
-        return "Future sight";
+        return "Future Sight";
     case Move::ROCK_SMASH:
-        return "Rock smash";
+        return "Rock Smash";
     case Move::WHIRLPOOL:
         return "Whirlpool";
     case Move::BEAT_UP:
-        return "Beat up";
+        return "Beat Up";
     default:
         return "Unknown move";
     }
